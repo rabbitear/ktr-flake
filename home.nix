fbd3ae017d5e8b6cee5f49631010cec8a3575c82
Jon <weatchu@gmail.com>
2025-10-19 19:41:46 -0800

    yoshi enhancements

--- home.nix
+++ home.nix
@@ -185,6 +185,18 @@ in
     };
   };
 
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
   programs.bash = {
     enable = true;
     enableCompletion = true;
