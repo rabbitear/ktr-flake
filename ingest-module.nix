989d57c8c65aec612f5eff2fbda69e5bc8d8817f
Jon Bradley <weatchu@gmail.com>
2025-10-23 06:43:54 -0800

    again, got rid of older commits

--- ingest-module.nix
+++ ingest-module.nix
@@ -71,34 +71,18 @@ let
         # -----------------------------------------------------------------
         # 2. Determine MIME type
         # -----------------------------------------------------------------
-<<<<<<< HEAD
         mime_type=$(file --brief --mime-type -- "$src" | cut -d'/' -f1)
-=======
-        mime=$(file -b --mime-type "$src")
-        echo mime is $mime
-        type=$${mime%%/*}          # e.g. text, image, application
-        subtype=$${mime#*/}        # e.g. plain, png, json
->>>>>>> c2c709e (successful build: Wed Oct 22 04:19:31 PM AKDT 2025)
-    
+
         # -----------------------------------------------------------------
         # 3. Build destination directory (type/subtype hierarchy)
         # -----------------------------------------------------------------
-<<<<<<< HEAD
         target_dir="$KB_ROOT/$mime_type"
-=======
-        if [[ $subtype == "$type" || -z "$subtype" ]]; then
-            target_dir="$KB_ROOT/$type"
-        else
-            target_dir="$KB_ROOT/$type/$subtype"
-        fi
->>>>>>> c2c709e (successful build: Wed Oct 22 04:19:31 PM AKDT 2025)
         mkdir -p "$target_dir"
-        echo target_dir $target_dir
+        echo "--- target_dir $target_dir"
     
         # -----------------------------------------------------------------
         # 4. Build a safe, unique destination file name
         # -----------------------------------------------------------------
-<<<<<<< HEAD
         orig_base=$(basename "$src")          # original file name
         nospace_base=$(echo "$orig_base" | sed 's/ /_/g')
         new_base=$(date +%s)_$nospace_base
@@ -110,43 +94,15 @@ let
         cp -a "$src" "$dest"
         echo "" >> $JOURNAL_FILE
         echo "[$orig_base](../$mime_type/$new_base)" >> $JOURNAL_FILE
-=======
-        # epoch="$(date +%s)"                     # Unix epoch seconds
-        # orig_base=$(basename "$src")          # original file name
-        # safe_base=$(sanitize_name "$orig_base")   # <-- NEW STEP
-        # ebase=$epoch/$safe_base
-        # dest=$target_dir/$ebase
-        # echo "dest is NOW: $dest"
-        # If, for any reason, the name already exists (extremely unlikely),
-        # append a counter before the original name.
-        # if [[ -e "$dest" ]]; then
-            # i=1
-            # while [[ -e $${target_dir}/$${epoch}_$${i}_$${safe_base} ]]; do ((i++)); done
-            # dest=$${target_dir}/$${epoch}_$${i}_$${safe_base}
-        # fi
-    
-        # -----------------------------------------------------------------
-        # 5. Copy the file (preserve metadata)
-        # -----------------------------------------------------------------
-        #echo 'cp -a "$src" "$dest"'
-        #echo "Added: "$dest"
-        echo WHATUP
->>>>>>> c2c709e (successful build: Wed Oct 22 04:19:31 PM AKDT 2025)
-    
+   
         # -----------------------------------------------------------------
         # 6. Stage the new file in the repo (optional but convenient)
         # -----------------------------------------------------------------
-<<<<<<< HEAD
         if [[ -d $KB_ROOT/.git ]]; then
           git -C "$KB_ROOT" add "$dest"
           echo " --> but do we really want to commit here?"
           git -C "$KB_ROOT" commit -m "Ingested: $dest"
         fi
-=======
-        #if [[ -d $KB_ROOT/.git ]]; then
-        #  (cd "$KB_ROOT" && git add "$dest" && git commit -m "Ingested: $dest")
-        #fi
->>>>>>> c2c709e (successful build: Wed Oct 22 04:19:31 PM AKDT 2025)
     done
   '';
 in {
