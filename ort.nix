af227d2c2939383dc368252061bd7863d04c8d81
Jon Bradley <weatchu@gmail.com>
2025-10-27 11:45:10 -0800

    successful build: Mon Oct 27 11:45:10 AM AKDT 2025

--- ort.nix
+++ ort.nix
@@ -7,15 +7,24 @@ pkgs.rustPlatform.buildRustPackage rec {
   src = pkgs.fetchFromGitHub {
     owner = "grahamking";
     repo = "ort";
-    rev = "master"; # replace with commit/tag for reproducible builds
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    # this rev commit should be before tests, its Oct27-2025.
+    #rev = "ff8aed89a444bf809c5a73e05fe1ede8095a4d13";
+    # this rev commit is of todays date Oct27, added doTest false below.
+    rev = "adb6c615d47c7d48601beaa5bffbeca55f5abaec";
+    #sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "sha256-CTXIH7qz/USrZ9C5/l/ODnSCu4oNrLrNnrDDfcZUNI8=";
   };
 
-  cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+  # Use the Cargo.lock inside the fetched source to resolve dependency versions:
+  cargoLock.lockFile = "${src}/Cargo.lock";
 
   nativeBuildInputs = [ pkgs.pkg-config pkgs.clang ];
   buildInputs = [ pkgs.openssl pkgs.zlib ];
-  cargoBuildFlags = [ "--release" ];
+
+  #cargoBuildFlags = [ "--release" ];
+
+  # don't run tests during the nix build (they require OPENROUTER_API_KEY)
+  doCheck = false;
 
   meta = with pkgs.lib; {
     description = "ort from grahamking/ort";
