983862062d4898e599d1177483819432a83fbc92
rabbitear <weatchu@gmail.com>
2025-11-03 21:20:01 -0900

    successful build: Mon Nov  3 09:20:00 PM AKST 2025

--- users+home/default.nix
+++ users+home/default.nix
@@ -34,6 +34,14 @@ in
     pkgs.nerd-fonts.fira-mono
     pkgs.nerd-fonts.fira-code
     pkgs.nerd-fonts.fantasque-sans-mono
+    pkgs.mdbook
+    pkgs.mdbook-emojicodes
+    pkgs.mdbook-epub
+    pkgs.mdbook-admonish
+    pkgs.mdbook-d2
+    pkgs.mdbook-graphviz
+    pkgs.mdbook-linkcheck
+    pkgs.mdbook-toc
   ];
   home = {
     username = "kreator";
@@ -221,7 +229,7 @@ in
       set -o vi
     '';
     shellAliases = {
-      @ = "hx";
+      "@" = "hx";
       e = "hx";
       ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
       "?" = "${duckduckgo-search}/bin/duckduckgo-search";
