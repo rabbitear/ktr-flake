64065d6ab79ef87a01a6645cea599ef5334325ec
rabbitear <weatchu@gmail.com>
2025-11-04 17:01:14 -0900

    successful build: Tue Nov  4 05:01:14 PM AKST 2025

--- users+home/sync-jrnl.nix
+++ users+home/sync-jrnl.nix
@@ -2,24 +2,24 @@
 # ^ that is the variable we're looking for that holds the path of the git repo, it exists in sops.
 {config, pkgs, ...}:
 {
-systemd.user = {
-  services.journal-sync = {
-    Unit.Description = "Sync journal repository";
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.git}/bin/git -C ~/.journal pull --ff-only";
-      TimeoutStopSec = 30;
+  systemd.user = {
+    services.journal-sync = {
+      Unit.Description = "Sync journal repository";
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.git}/bin/git -C${config.home.homeDirectory}/.journal pull --ff-only";
+        TimeoutStopSec = 30;
+      };
+    };
+  
+    timers.journal-sync = {
+      Unit.Description = "Timer for journal repo syncing";
+      Timer = {
+        OnBootSec = "1m";
+        OnUnitActiveSec = "30m";
+        AccuracySec = "5m";
+      };
+      Install.WantedBy = ["timers.target"];
     };
   };
-
-  timers.journal-sync = {
-    Unit.Description = "Timer for journal repo syncing";
-    Timer = {
-      OnBootSec = "1m";
-      OnUnitActiveSec = "30m";
-      AccuracySec = "5m";
-    };
-    Install.WantedBy = ["timers.target"];
-  };
-};
-
+}
