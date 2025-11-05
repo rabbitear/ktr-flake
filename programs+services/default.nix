5507209de211c3386be62e9c8a8b7bd8166c08e3
Jon Bradley <weatchu@gmail.com>
2025-11-04 18:05:51 -0900

    successful build: Tue Nov  4 06:05:51 PM AKST 2025

--- programs+services/default.nix
+++ programs+services/default.nix
@@ -68,6 +68,7 @@
     };
   };
 
+  programs.adb.enable = true;
 
   # Networking
   # Enable SSH access in from Tailscale network 22
@@ -100,7 +101,7 @@
     isNormalUser = true;
     description = "Jon";
     hashedPasswordFile = config.sops.secrets.kreator.path;
-    extraGroups = [ "networkmanager" "wheel" "flatpak" "video" "kvm" ];
+    extraGroups = [ "networkmanager" "wheel" "flatpak" "video" "kvm" "adbusers" ];
     openssh.authorizedKeys.keys = [
       # Add SSH public keys here.
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzV4VriIYwSvx8e3Pq2hKjJDPsyj1hJAgrsiXJG/BVR kreator@theshack"
