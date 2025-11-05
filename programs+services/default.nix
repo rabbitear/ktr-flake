d0ffd3281673d68086b06d6d410e6db04cbc5126
Jon Bradley <weatchu@gmail.com>
2025-11-04 18:03:41 -0900

    successful build: Tue Nov  4 06:03:41 PM AKST 2025

--- programs+services/default.nix
+++ programs+services/default.nix
@@ -100,7 +100,7 @@
     isNormalUser = true;
     description = "Jon";
     hashedPasswordFile = config.sops.secrets.kreator.path;
-    extraGroups = [ "networkmanager" "wheel" "flatpak" "video" ];
+    extraGroups = [ "networkmanager" "wheel" "flatpak" "video" "kvm" ];
     openssh.authorizedKeys.keys = [
       # Add SSH public keys here.
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzV4VriIYwSvx8e3Pq2hKjJDPsyj1hJAgrsiXJG/BVR kreator@theshack"
