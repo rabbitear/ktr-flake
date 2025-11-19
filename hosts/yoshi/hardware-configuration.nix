afeb2aaf965c7e3572b4fdb018e5de0c36c56fa1
Jon <weatchu@gmail.com>
2025-11-18 16:21:41 -0900

    successful build: Tue Nov 18 04:21:41 PM AKST 2025

--- hosts/yoshi/hardware-configuration.nix
+++ hosts/yoshi/hardware-configuration.nix
@@ -13,10 +13,10 @@
   boot.kernelModules = [ "kvm-amd" "fuse" "coretemp" "nvidia" ];
   boot.extraModulePackages = [ ];
 
-  boot.kernelParams = [
-    "nvidia-drm.modeset=1"   # keep this
-    "nvidia-drm.fbdev=0"   # ← remove or comment out
-  ];
+  # boot.kernelParams = [
+  #   "nvidia-drm.modeset=1"   # keep this
+  #   "nvidia-drm.fbdev=0"   # ← remove or comment out
+  # ];
 
   
   nixpkgs.config.cudaSupport = true;
