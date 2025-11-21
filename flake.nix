3a6814ea4c32bc1b9ff8dc9eb77972e8602fde22
Jon Bradley <weatchu@gmail.com>
2025-11-20 21:28:46 -0900

    openwebui running on sasha

--- flake.nix
+++ flake.nix
@@ -187,6 +187,7 @@
           ./programs+services/chromecast.nix
           ./programs+services/n8n-sasha.nix
           ./programs+services/searx.nix
+          ./programs+services/openwebui.nix
           ./programs+services/graphics-programs.nix
           sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
