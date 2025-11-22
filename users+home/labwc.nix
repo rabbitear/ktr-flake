26fa4e8cabae07c5108c870a991779a573c4af43
Jon Bradley <weatchu@gmail.com>
2025-11-22 13:52:08 -0900

    successful build: Sat Nov 22 01:52:08 PM AKST 2025

--- users+home/labwc.nix
+++ users+home/labwc.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     swaybg
     fuzzel
+    foot
     grim
     slurp
     libnotify
@@ -40,6 +41,55 @@
       width = 300;
     };
   };
+
+
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        term = "xterm-256color";
+        font = "Monospace:size=11";
+        pad = "4x4";
+        dpi-aware = "yes";
+        initial-window-size-chars = "80x25";
+        initial-window-mode = "windowed";
+      };
+      colors = {
+        foreground = "c0caf5";
+        background = "1a1b26";
+        
+        ## Normal/regular colors (color palette 0-7)
+        regular0 = "15161E";  # black
+        regular1 = "f7768e";  # red
+        regular2 = "9ece6a";  # green
+        regular3 = "e0af68";  # yellow
+        regular4 = "7aa2f7";  # blue
+        regular5 = "bb9af7";  # magenta
+        regular6 = "7dcfff";  # cyan
+        regular7 = "a9b1d6";  # white
+        
+        ## Bright colors (color palette 8-15)
+        bright0 = "414868";   # bright black
+        bright1 = "f7768e";   # bright red
+        bright2 = "9ece6a";   # bright green
+        bright3 = "e0af68";   # bright yellow
+        bright4 = "7aa2f7";   # bright blue
+        bright5 = "bb9af7";   # bright magenta
+        bright6 = "7dcfff";   # bright cyan
+        bright7 = "c0caf5";   # bright white
+        
+        ## dimmed colors (see foot.ini(5) man page)
+        dim0 = "ff9e64";
+        dim1 = "db4b4b";
+       
+        alpha = 0.9;
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+    };
+  };
+
 
   wayland.windowManager.labwc = {
     enable = true;
@@ -161,7 +211,7 @@
             menuId = "openbox_pipe_menu";
             icon = "xterm";
             label = "Apps...";
-            execute = "${pkgs.labwc-menu-generator}/bin/labwc-menu-generator --desktop --icons --pipemenu --terminal-prefix=${pkgs.foot}/bin/foot --window-size-chars=80x25 --font=monospace:size=11";
+            execute = "${pkgs.labwc-menu-generator}/bin/labwc-menu-generator --desktop --icons --pipemenu --terminal-prefix=${pkgs.foot}/bin/foot";
           }
           {
             label = "Alacrity";
@@ -253,7 +303,7 @@
                 menuId = "openbox_pipe_menu";
                 icon = "menu";
                 label = "Apps...";
-                execute = "${pkgs.labwc-menu-generator}/bin/labwc-menu-generator --desktop --icons --pipemenu --terminal-prefix=${pkgs.foot}/bin/foot --window-size-chars=80x25 --font=monospace:size=11";
+                execute = "${pkgs.labwc-menu-generator}/bin/labwc-menu-generator --desktop --icons --pipemenu --terminal-prefix=${pkgs.foot}/bin/foot";
               }
             ];
           }
