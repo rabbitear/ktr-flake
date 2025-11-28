bfbc2bed022fc0d9d94588928799f2ff81b78134
Jon <weatchu@gmail.com>
2025-11-27 20:38:03 -0900

    Adding llamacpp.nix to home manager

---
+++ users+home/llamacpp.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+let
+  llama-cpp = pkgs.llama-cpp.overrideAttrs(attrs: rec {
+    version = "5869";
+    src = pkgs.fetchFromGitHub {
+      owner = "ggml-org";
+      repo = "llama.cpp";
+      tag = "b${version}";
+      hash = "sha256-sQK5OHuzRaT5wiz6+6ZBQxpzCLhYjdWy1ZsPcLrvMe4=";
+    };
+  });
+in
+{
+  home.packages = [
+    (llama-cpp.override { cudaSupport = true; })
+  ];
+}
