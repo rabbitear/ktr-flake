6b4f72b217949ebcffa09b2a91d97e6d4af224bd
Jon Bradley <weatchu@gmail.com>
2025-12-02 05:07:34 -0900

    successful build: Tue Dec  2 05:07:34 AM AKST 2025

--- hosts/sasha/hardware-configuration.nix
+++ hosts/sasha/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "bluetooth" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "bluetooth" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
