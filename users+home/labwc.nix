1440fad16cb000ff0d20a9c1e3d3c2ebce55bba5
Jon Bradley <weatchu@gmail.com>
2025-12-02 06:19:52 -0900

    successful build: Tue Dec  2 06:19:52 AM AKST 2025

--- users+home/labwc.nix
+++ users+home/labwc.nix
@@ -7,6 +7,7 @@ let
       #       if it is clicked on (notify action) than view the photo. 
       GEOMETRY="$(slurp)"
       grim -g "$GEOMETRY" - | wl-copy
+      notify-send "screenshot" "took screenshot"
     '';  
   };
   runraisehide = pkgs.writeShellApplication {
