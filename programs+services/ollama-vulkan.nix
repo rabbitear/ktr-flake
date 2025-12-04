a6043ee558a4ac02016c2390ae123da3acedcbfb
Jon Bradley <weatchu@gmail.com>
2025-12-04 11:56:11 -0900

    add ollama vulkan, rocm broken for now

---
+++ programs+services/ollama-vulkan.nix
@@ -0,0 +1,17 @@
+# this is the ollama service.
+
+{pkgs, ...}:
+
+{
+  services.ollama = {
+    enable = true;
+    host = "0.0.0.0";
+    acceleration = "vulkan";
+    openFirewall = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.ollama-vulkan
+    pkgs.llama-cpp-vulkan
+  ];
+}
