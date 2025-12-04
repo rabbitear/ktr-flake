7eca24654948ba19cdcc54405be2834542ebf1cf
Jon Bradley <weatchu@gmail.com>
2025-12-04 11:50:03 -0900

    successful build: Thu Dec  4 11:50:03 AM AKST 2025

--- programs+services/ollama-rocm.nix
+++ programs+services/ollama-rocm.nix
@@ -1,15 +1,20 @@
 # this is the ollama service.
 
-{pkgs, lib, ...}:
+{pkgs, ...}:
 
 {
   services.ollama = {
     enable = true;
     host = "0.0.0.0";
-    loadModels = [
-      "deepseek-r1:1.5b"
-    ];
+    #loadModels = [
+    #  "deepseek-r1:1.5b"
+    #];
     acceleration = "rocm";
     openFirewall = true;
   };
+
+  environment.systemPackages = [
+    pkgs.ollama-rocm
+    pkgs.llama-cpp-rocm
+  ];
 }
