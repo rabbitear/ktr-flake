54563d9c029d7d7c972075cafa2ed5cc322d5d16
Jon Bradley <weatchu@gmail.com>
2025-12-14 20:52:30 -0900

    successful build: Sun Dec 14 08:52:30 PM AKST 2025

--- my-addition/helix-dev/devenv.nix
+++ my-addition/helix-dev/devenv.nix
@@ -30,13 +30,13 @@
     build.exec = "cd helix && cargo build --release";
     run.exec = "cd helix && ./target/release/hx --version";
     test.exec = "cd helix && cargo test";
-    hx.exec = "cd helix && ./target/release/hx";
+    custom-hx.exec = "cd helix && ./target/release/hx";
     test-ai.exec = ''
       echo "Testing AI setup..."
       echo "HANDLER: $HANDLER"
       echo "COPILOT_API_KEY set: $(if [ -n "$COPILOT_API_KEY" ]; then echo "Yes"; else echo "No"; fi)"
       echo "helix-gpt available: $(which helix-gpt)"
-      echo "To test in Helix: run 'hx test.rs', type inside {}, check for AI completions"
+      echo "To test in Helix: run 'custom-hx test.rs', type inside {}, check for AI completions"
     '';
   };
 
