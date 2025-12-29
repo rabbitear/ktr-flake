4e15dd2503e72508095744d03d7692b76cd69915
Jon Bradley <weatchu@gmail.com>
2025-12-28 21:06:23 -0900

    successful build: Sun Dec 28 09:06:23 PM AKST 2025

--- users+home/o.nix
+++ users+home/o.nix
@@ -6,10 +6,11 @@ let
     text = ''
       #!/usr/bin/env bash
       model=''${1:-"tngtech/tng-r1t-chimera:free"}
-      [[ ! -d "$HOME"/.o ]] && mkdir "$HOME"/.o
+      outdir="$HOME/.journal/o"
+      [[ ! -d "$outdir" ]] && mkdir "$HOME"/.o
       # Check if input is from pipe/redirection
-      outputfile="$HOME/.o/$(date +%F_%H_%M_%S)_output.md"
-      inputfile="$HOME/.o/$(date +%F_%H_%M_%S)_input.md"
+      outputfile="$outdir/$(date +%F_%H_%M_%S)_output.md"
+      inputfile="$outdir/$(date +%F_%H_%M_%S)_input.md"
       if [[ -t 0 ]]; then
         # Interactive mode (terminal input)
         echo -e "\e[0m 🦉📥 \e[0;31m**$model:** \e[1;31mEnter \e[0;33mText \e[1;31mHere\e[0m 📡📝" >&2
