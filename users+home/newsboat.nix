070c1d67d0198622956f6967bcf2762516da0a46
rabbitear <rabbitear@pm.me>
2026-01-22 18:52:10 -0900

    successful build: Thu Jan 22 06:52:10 PM AKST 2026

--- users+home/newsboat.nix
+++ users+home/newsboat.nix
@@ -7,6 +7,35 @@
       miniflux-url "http://sasha:3003/"
       miniflux-login "kreator"
       miniflux-password "some-pass"
+
+      # unbind keys
+      #unbind-key ENTER
+      unbind-key j
+      unbind-key k
+      unbind-key J
+      unbind-key K
+      unbind-key l
+      unbind-key h
+      
+      # bind keys - vim style
+      bind-key j down
+      bind-key k up
+      bind-key l open
+      bind-key h quit
+      
+      # solarized
+      color background         default   default
+      color listnormal         default   default
+      color listnormal_unread  default   default
+      color listfocus          black     cyan
+      color listfocus_unread   black     cyan
+      color info               default   black
+      color article            default   default
+      
+      # highlights
+      highlight article "^(Title):.*$" blue default
+      highlight article "https?://[^ ]+" red default
+      highlight article "\\[image\\ [0-9]+\\]" green default
     '';
   };
 }
