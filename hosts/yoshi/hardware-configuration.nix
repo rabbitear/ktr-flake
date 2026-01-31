faa6742d79a47fa182054d9d34044e3d8f181597
Jon <weatchu@gmail.com>
2026-01-30 18:59:35 -0900

    successful build: Fri Jan 30 06:59:35 PM AKST 2026

--- hosts/yoshi/hardware-configuration.nix
+++ hosts/yoshi/hardware-configuration.nix
@@ -19,7 +19,7 @@
   boot.extraModulePackages = [ ];
 
   boot.kernelParams = [
-    "nvidia-drm.modeset=1"   # keep this
+    #"nvidia-drm.modeset=1"   # keep this
     "nvidia-drm.fbdev=0"   # ← remove or comment out
   ];
 
